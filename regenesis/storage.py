90c3d72545bec2820636f52c4b0d0aa385b446a3
Friedrich Lindenberg <friedrich@pudo.org>
2013-03-26 15:26:03 +0100

    Fix paths with prefix.

--- regenesis/storage.py
+++ regenesis/storage.py
@@ -2,7 +2,7 @@ import os
 import json
 from regenesis.core import app
 
-def cube_path(catalog, cube_name, ext):
+def cube_path(catalog, cube_name, ext='raw'):
     return os.path.join(
             app.config.get('DATA_DIRECTORY'),
             catalog,
@@ -10,10 +10,10 @@ def cube_path(catalog, cube_name, ext):
             )
 
 def exists_raw(catalog, cube_name):
-    return os.path.isfile(cube_path(catalog, cube_name, 'raw'))
+    return os.path.isfile(cube_path(catalog, cube_name))
 
 def store_cube_raw(catalog, cube_name, cube_data):
-    path = cube_path(catalog, cube_name, 'raw')
+    path = cube_path(catalog, cube_name)
     if not os.path.isdir(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path))
     fh = open(path, 'wb')
@@ -21,13 +21,13 @@ def store_cube_raw(catalog, cube_name, cube_data):
     fh.close()
 
 def load_cube_raw(catalog, cube_name):
-    fh = open(cube_path(catalog, cube_name, 'raw'), 'rb')
+    fh = open(cube_path(catalog, cube_name), 'rb')
     data = fh.read().decode('utf-8')
     fh.close()
     return data
 
-def dump_cube_json(cube):
-    fh = open(cube_path(cube.name, 'json'), 'wb')
+def dump_cube_json(catalog, cube):
+    fh = open(cube_path(catalog, cube.name, 'json'), 'wb')
     json.dump(cube, fh, cls=JSONEncoder, indent=2)
     fh.close()
 
