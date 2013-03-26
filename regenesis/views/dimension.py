72b37977f753d5a26635b9f6029601fac9619459
Friedrich Lindenberg <friedrich@pudo.org>
2013-03-26 17:06:16 +0100

    Always use catalog element.

--- regenesis/views/dimension.py
+++ regenesis/views/dimension.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, render_template
 
 import sqlaload as sl
-from regenesis.core import app, engine
+from regenesis.core import app, engine, get_catalog
 from regenesis.database import dimension_table, value_table
 
 blueprint = Blueprint('dimension', __name__)
 
 @blueprint.route('/<catalog>/dimensions')
 def index(catalog):
+    catalog = get_catalog(catalog)
     dimensions = sl.find(engine, dimension_table,
             order_by='title_de')
     return render_template('dimension/index.html',
@@ -16,6 +17,7 @@ def index(catalog):
 
 @blueprint.route('/<catalog>/dimensions/<name>')
 def view(catalog, name):
+    catalog = get_catalog(catalog)
     dimension = sl.find_one(engine, dimension_table, name=name)
     values = sl.find(engine, value_table, dimension_name=name, 
                 order_by='title_de')
--- regenesis/views/statistic.py
+++ regenesis/views/statistic.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, render_template
 
 import sqlaload as sl
-from regenesis.core import app, engine
+from regenesis.core import app, engine, get_catalog
 from regenesis.database import statistic_table
 
 blueprint = Blueprint('statistic', __name__)
 
 @blueprint.route('/<catalog>/statistics')
 def index(catalog):
+    catalog = get_catalog(catalog)
     statistics = sl.find(engine, statistic_table,
             order_by='title_de')
     return render_template('statistic/index.html',
@@ -16,6 +17,7 @@ def index(catalog):
 
 @blueprint.route('/<catalog>/statistics/<name>')
 def view(catalog, name):
+    catalog = get_catalog(catalog)
     statistic = sl.find_one(engine, statistic_table, name=name)
     return render_template('statistic/view.html',
             catalog=catalog,
