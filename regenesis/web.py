bebae1a5c8d29ecfb1b880be013a1f160e39a6b5
Friedrich Lindenberg <friedrich@pudo.org>
2013-03-26 12:57:04 +0100

    Basic HTML output rendering.

--- regenesis/web.py
+++ regenesis/web.py
@@ -1,4 +1,11 @@
+from flask import render_template, Response, request
+from flask import session, redirect, flash, Markup
 
+from regenesis.core import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 
