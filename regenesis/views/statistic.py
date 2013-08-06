750b88484105959d361c79c83fd0dd711cb27486
Friedrich Lindenberg <friedrich@pudo.org>
2013-08-06 19:17:49 +0200

    List cubes on statistic page.

--- regenesis/views/statistic.py
+++ regenesis/views/statistic.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 from collections import defaultdict
 
 from regenesis.core import app, engine, get_catalog
+from regenesis.views.util import dimension_type_text
 from regenesis.database import statistic_table, cube_table
 from regenesis.queries import get_dimensions
 from regenesis.views.util import parse_description
@@ -31,13 +32,25 @@ def view(catalog, name):
     for cube in get_cubes(name):
       cube['dimensions'] = get_dimensions(cube['name'])
       for dim in cube['dimensions']:
+        dim['show'] = True
         dims[dim['dim_name']] += 1
+        dim['type_text'] = dimension_type_text(dim['dim_measure_type'])
         if dim['dim_measure_type'].startswith('K-REG'):
-            cube['admlevel'] = (ADM_RANKS[dim['dim_name']], dim['dim_title_de'])
+            cube['admlevel'] = (ADM_RANKS[dim['dim_name']], dim['dim_title_de'], dim['dim_name'])
+            dim['show'] = False
       cube['stand'] = cube['provenance'].split('hat am')[-1].split('um')[0].strip()
       cubes.append(cube)
+    common = [d for d, i in dims.items() if i == len(cubes)]
+    commons = {}
+    for cube in cubes:
+        for dim in cube['dimensions']:
+            if dim['dim_name'] in common:
+                dim['show'] = False
+                commons[dim['dim_name']] = dim
     return render_template('statistic/view.html',
                            catalog=catalog,
                            desc=desc,
                            cubes=cubes,
+                           common=commons.values(),
+                           has_common=len(common) > 0,
                            statistic=statistic)
