4e0887fcdbfa11be28a99bda8567253e4f015f72
Friedrich Lindenberg <friedrich@pudo.org>
2013-08-14 19:34:56 +0200

    Quiet upload.

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-s3cmd sync -c s3config .s3ignore --acl-public --guess-mime-type build/* s3://regenesis.pudo.org
+s3cmd sync -c s3config .s3ignore --acl-public --guess-mime-type --no-progress build/* s3://regenesis.pudo.org
