a82f1d21082f2f312166b58d1e78e69a89e0e0a6
Friedrich Lindenberg <friedrich@pudo.org>
2013-11-04 20:05:02 +0100

    use external slugification

--- regenesis/util.py
+++ regenesis/util.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from hashlib import sha1
-from unicodedata import normalize as ucnorm, category
-
+from slugify import slugify
 
 def make_key(*a):
     parts = []
@@ -25,24 +24,3 @@ def flatten(d, sep='_'):
         else:
             out[k] = v
     return out
-
-
-def slugify(text):
-    if not isinstance(text, unicode):
-        text = unicode(text)
-    text = text.lower()
-    decomposed = ucnorm('NFKD', text)
-    filtered = []
-    for char in decomposed:
-        cat = category(char)
-        if char == "'" or cat.startswith('M') or cat.startswith('S'):
-            continue
-        elif cat.startswith('L') or cat.startswith('N'):
-            filtered.append(char)
-        else:
-            filtered.append('-')
-    text = u''.join(filtered)
-    while '--' in text:
-        text = text.replace('--', '-')
-    text = text.strip()
-    return ucnorm('NFKC', text).encode('ascii', 'ignore')
